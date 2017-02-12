59b24158b550128011eb81872ea0a78b6e5aada4
吉良 健太郎 <kirakentaro@kira-no-MacBook-Air.local>
2017-02-12 22:03:57 +0900

    change js

--- www/WakeUpMeTimer.js
+++ www/WakeUpMeTimer.js
@@ -1,6 +1,7 @@
-var exec = require('cordova/exec');
-
-exports.setTimer = function(countDownTime, success, error) {
+var setTimer = {
+    set: function(countDownTime, success, error) {
     console.log("called setTimer");
     exec(success, error, "WakeUpMeTimer", "setTimer", [countDownTime]);
+    }
 };
+module.exports = alarm;setTimer
